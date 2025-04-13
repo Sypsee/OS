46e3e93864e896d1a426e901747ac1a217c43d94
Sypsee <vitthalsoni6298@gmail.com>
2025-04-13 20:44:15 +0530

    Disk Read

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,4 +1,4 @@
-; this is a directive, it will only give a clue to the assembler & not get translated to machine code
+; A directive will only give a clue to the assembler & not get translated to machine code
 ; any label starting from . is a local label in nasm
 ; general layout for real mode -> segment:offset
 ; segment * 16 + offset = physical address
@@ -42,7 +42,174 @@ ebr_volume_label:           db 'SYPSE OS   '        ; 11 bytes, padded with spac
 ebr_system_id:              db 'FAT12   '           ; 8 bytes
 
 start:
-    jmp main
+    mov ax, 0
+    mov ds, ax ; why not just mov ds, 0? -> this is not allowed and one must only move from general purpose registers
+    mov es, ax
+
+    mov ss, ax ; set stack-segment to 0x0000
+    mov sp, 0x7C00 ; set stack-pointer to 0x7C00 as stack grows down this makes sure we dont overwrite our os
+
+    push es
+    push word .after
+    retf
+
+.after:
+    mov [ebr_drive_number], dl
+
+    mov si, msg_loading
+    call print
+
+    push es
+    mov ah, 08h
+    jc floppy_error
+    pop es
+
+    and cl, 0x3f ; remove top 2 bits
+    xor ch, ch
+    mov [bdb_sectors_per_track], cx ; sector count
+
+    inc dh
+    mov [bdb_heads], dh ; head count
+
+    ; compute LBA of root dir = reserved + fats * sectors_per_fat
+    mov ax, [bdb_sectors_per_fat]
+    mov bl, [bdb_fat_count]
+    xor bh, bh
+    mul bx ; ax = (fats * sectors_per_fat)
+    add ax, [bdb_reserved_sectors] ; ax = LBA of root dir
+    push ax
+
+    ; compute size of root dir = (32 * number_of_entities) / bytes_per_sector
+    mov ax, [bdb_sectors_per_fat]
+    shl ax, 5 ; ax *= 32
+    xor dx, dx
+    div word [bdb_bytes_per_sector] ; number of bytes we need to read
+
+    test dx, dx ; if dx != 0, then add 1
+    jz .root_dir_after
+    inc ax ; division remainder != 0, add 1
+
+.root_dir_after:
+    ; read root dir
+    mov cl, al ; cl = number of sectors to read = size of roto dir
+    pop ax ; ax = LBA of root dir
+    mov dl, [ebr_drive_number] ; dl = drive number
+    mov bx, buffer ; es:bx = buffer
+    call disk_read
+
+    ; search for kernel.bin
+    xor bx, bx
+    mov di, buffer
+
+.search_kernel:
+    mov si, file_kernel_bin
+    mov cx, 11 ; compare up to 11 chars
+    push di
+    ; repe -> repeats a string instruction while the operands are equal (zero falg = 1), or until cx reaches 0
+    ; cx is decremented each iteration
+    ; cmpsb -> compares 2 bytes located in memory at address ds:si and es:di
+    ; si & di are incremented (if direction flag = 0) or decremented (if direction flag = 1)
+    repe cmpsb
+    pop di
+    je .found_kernel
+
+    add di, 32
+    inc bx
+    cmp bx, [bdb_dir_entries_count]
+    jl .search_kernel
+
+    ; kernel not found
+    jmp kernel_not_found_error
+
+.found_kernel:
+    ; di should have the address to the entry
+    mov ax, [di + 26] ; first logical cluster field (offset 26)
+    mov [kernel_cluster], ax
+
+    ; load FAT from disk into memory
+    mov ax, [bdb_reserved_sectors]
+    mov bx, buffer
+    mov cl, [bdb_sectors_per_fat]
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    ; read kernel and process FAT chain
+    mov bx, KERNEL_LOAD_SEGMENT
+    mov es, bx
+    mov bx, KERNEL_LOAD_OFFSET
+
+.load_kernel_loop:
+    ; Read next cluster
+    mov ax, [kernel_cluster]
+    add ax, 31 ; first cluster = (kernel_cluster - 2) * sectors_per_cluster + start_sector
+               ; start sector = reserved + fats + root_dir_size = 1 + 18 + 134 = 33
+    
+    mov cl, 1
+    mov dl, [ebr_drive_number]
+    call disk_read
+
+    add bx, [bdb_bytes_per_sector]
+
+    ; compute location of next cluster
+    mov ax, [kernel_cluster]
+    mov cx, 3
+    mul cx
+    mov cx, 2
+    div cl ; ax = index of entry in FAT, dx = cluster mod 2
+
+    mov si, buffer
+    add si, ax
+    mov ax, [ds:si] ; read entry from FAT table at index ax
+
+    or dx, dx
+    jz .even
+
+.odd:
+    shr ax, 4
+    jmp .next_cluster_after
+
+.even:
+    and ax, 0x0FFF
+
+.next_cluster_after:
+    cmp ax, 0x0FF8 ; end of chain
+    jae .read_finish
+
+    mov [kernel_cluster], ax
+    jmp .load_kernel_loop
+
+.read_finish:
+    ; boot device in dl
+    mov dl, [ebr_drive_number]
+    mov ax, KERNEL_LOAD_SEGMENT
+    mov ds, ax
+    mov es, ax
+
+    jmp KERNEL_LOAD_SEGMENT:KERNEL_LOAD_OFFSET
+
+    jmp wait_key_and_reboot
+
+    cli
+    hlt
+
+floppy_error:
+    mov si, msg_read_failed
+    call print
+    jmp wait_key_and_reboot
+
+kernel_not_found_error:
+    mov si, msg_kernel_not_found
+    call print
+    jmp wait_key_and_reboot
+
+wait_key_and_reboot:
+    mov ah, 0
+    int 16h ; wait for keypress
+    jmp 0FFFFh:0 ; jump to the beginning of BIOS, restart
+
+.halt:
+    cli ; disable interupts
+    hlt
 
 ;
 ; Prints a message to screen
@@ -61,53 +228,17 @@ print:
     jz .done ; jumps if zero flag is set
 
     ; call bios interrupt to print char
-    mov ah, 0x0e
+    mov ah, 0x0E
     mov bh, 0
     int 0x10
 
     jmp .loop
 
 .done:
+    pop bx
     pop ax
     pop si
-    pop bx
     ret
-
-main:
-    mov ax, 0
-    mov ds, ax ; why not just mov ds, 0? -> this is not allowed and one must only move from general purpose registers
-    mov es, ax
-
-    mov ss, ax ; set stack-segment to 0x0000
-    mov sp, 0x7C00 ; set stack-pointer to 0x7C00 as stack grows down this makes sure we dont overwrite our os
-
-    mov [ebr_drive_number], dl
-    mov ax, 1
-    mov cl, 1
-    mov bx, 0x7E00
-
-    call disk_read
-
-    mov si, msg_hello
-    call print
-
-    cli
-    hlt
-
-floppy_error:
-    mov si, msg_read_failed
-    call print
-    jmp wait_key_and_reboot
-
-wait_key_and_reboot:
-    mov ah, 0
-    int 16h ; wait for keypress
-    jmp 0FFFFh:0 ; jump to the beginning of BIOS, restart
-
-.halt:
-    cli ; disable interupts
-    jmp .halt
-
 
 ;
 ; Disk routines
@@ -187,12 +318,11 @@ disk_read:
 .done:
     popa
 
-    push di
-    push dx
-    push cx
-    push bx
-    push ax
-
+    pop di
+    pop dx
+    pop cx
+    pop bx
+    pop ax
     ret
 
 ;
@@ -209,9 +339,15 @@ disk_reset:
     popa
     ret
 
-msg_hello: db 'Hello world!', ENDL, 0
-msg_read_failed: db 'Failed to read from disk!', ENDL, 0
-msg_debug: db 'Debug here!', ENDL, 0
+msg_loading: db 'Reading Disk...', ENDL, 0
+msg_read_failed: db 'Disk read failed!', ENDL, 0
+msg_kernel_not_found: db 'Failed to locate KERNEL.BIN!', ENDL, 0
+file_kernel_bin: db 'KERNEL  BIN'
+kernel_cluster: dw 0
+
+; equ, this would not use any memory and would be replaced by the value at assembling time
+KERNEL_LOAD_SEGMENT equ 0x2000
+KERNEL_LOAD_OFFSET equ 0
 
 ; times number instruction/data (directive) -> repeats a instruction n number of times
 ; $ -> special sybmol which is equal to the memory offset of the current line
@@ -221,3 +357,5 @@ times 510-($-$$) db 0
 
 ; -> writes given bytes (2 byte value, encoding -> little-endian) to the assembled binary file
 dw 0AA55h ; magic number
+
+buffer:
